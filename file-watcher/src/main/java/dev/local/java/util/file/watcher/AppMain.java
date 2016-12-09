22e4bfdcbbe476292c1c05fa5938d251435e6055
Dmitri-Korolkov <d.korolkov88@gmail.com>
2016-12-09 12:56:09 +0300

    update file watcher

--- file-watcher/src/main/java/dev/local/java/util/file/watcher/AppMain.java
+++ file-watcher/src/main/java/dev/local/java/util/file/watcher/AppMain.java
@@ -1,7 +1,13 @@
 package dev.local.java.util.file.watcher;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class AppMain {
 
@@ -9,6 +15,40 @@ public class AppMain {
 
   public static void main(String[] args) {
 
+    File input = new File("input");
+    File output = new File("output");
+
+    try {
+
+      if (!input.exists()) input.mkdir();
+      if (!output.exists()) output.mkdir();
+
+      WatchService watcher = FileSystems.getDefault().newWatchService();
+      input.toPath().register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+      while (true) {
+
+        WatchKey key = watcher.take();
+
+        for (WatchEvent<?> event : key.pollEvents()) {
+
+          WatchEvent.Kind<?> kind = event.kind();
+
+          @SuppressWarnings("unchecked")
+          WatchEvent<Path> ev = (WatchEvent<Path>) event;
+
+          log.info("{}: {}", kind.name(), ev.context());
+
+          FileUtils.copyFile(
+                  new File(input.getPath() + File.separator + ev.context()), new File(output.getAbsolutePath()
+                          + File.separator + String.valueOf(System.currentTimeMillis() + "-" + ev.context())));
+        }
+
+        if (!key.reset()) break;
+      }
+    } catch (Exception e) {
+      log.error("{}", e);
+    }
   }
 
 }
